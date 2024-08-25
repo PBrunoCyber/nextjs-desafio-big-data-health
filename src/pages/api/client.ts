a17b500af6cb2ff5495c75fee37bd2334fdbb5d3
Paulo Bruno <paulo.bf111@gmail.com>
2024-08-25 07:16:39 -0300

    Client url updated

--- src/pages/api/client.ts
+++ src/pages/api/client.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const clientAxios = axios.create({
-  baseURL: `http://localhost:3000/api/`
+  baseURL: `http://localhost:3000/api/` || `https://nextjs-desafio-big-data-health.vercel.app/api/`
 })
 
 export { clientAxios }
